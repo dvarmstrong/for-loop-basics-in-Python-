d12dd6df86aa0edb0f28f1b65c35a27bdd395df7
D. Armstrong <61880632+dvarmstrong@users.noreply.github.com>
2021-10-25 15:35:48 -0400

    Add files via upload

---
+++ for_loop_basic1.py
@@ -0,0 +1,58 @@
+# For loops basics in Python
+# Uncomment loops to run the results
+
+
+
+
+
+# Prints all integers from 0 to 150
+# for x in range(0, 151 ):
+#     print(x)
+
+# Multiples of five : Prints all the multiples of 5 from 5 - 1000
+# def new_func():
+#         for x in range(5,1000+1,5):
+#             print(x)
+
+# new_func()
+
+
+# Print integers 1 -100. If divisible by 5 print "Coding"
+# If divisible by 10 print "Coding Dojo"
+# for x in range(1,100+1):
+#     if x % 5 == 0:
+#         print("Coding")
+#     if x % 10 == 0:
+#         print("Coding Dojo")
+#     else:
+#         print(x)
+
+# add odd intergers from 0 -500000 and print the final sum 
+# total = 0 
+# for x in range(500000):
+#         if x % 2 == 1:
+#             total = total + x
+#             print(total)
+
+
+# Print positive number starting at 2018 counting down by 4's 
+# for i in range (2018, 2, -4,):
+#     print(i)
+
+
+# Set three variables
+# lowNum:
+# highNum:
+# multi:
+# Starting at the lowNum
+# and going through highNum 
+# print only the integerss that
+# are a multiple of mult
+
+# lowNum = 4
+# highNum = 10
+# mult = 2
+
+# for i in range(lowNum,highNum,mult):
+#     print(i)
+
